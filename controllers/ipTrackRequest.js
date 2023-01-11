79b96ae1f1a91af0a0378cbd6b89dddcd65ce4df
Javier Lopez <javierlopez_85@hotmail.com>
2023-01-10 23:35:59 -0300

    routes and controllers files added

---
+++ controllers/ipTrackRequest.js
@@ -0,0 +1,48 @@
+const { request, response } = require('express');
+
+// Provisional data, this data could be erased.
+const provitionalData = {
+  ip: "8.8.8.8",
+  fromController: true,
+  location: {
+      country: "US",
+      region: "California",
+      city: "Mountain View",
+      lat: 37.40599,
+      lng: -122.078514,
+      postalCode: "94043",
+      timezone: "-07:00",
+      geonameId: 5375481
+  },
+  domains: [
+      "0d2.net",
+      "003725.com",
+      "0f6.b0094c.cn",
+      "007515.com",
+      "0guhi.jocose.cn"
+  ],
+  as: {
+      asn: 15169,
+      name: "Google LLC",
+      route: "8.8.8.0/24",
+      domain: "https://about.google/intl/en/",
+      type: "Content"
+  },
+  isp: "Google LLC"
+}
+
+const ipTrackRequest = ( req = request, res = response ) => {
+
+  const ipAddressReceived = req.params.ipaddress;
+
+  res.json( {...provitionalData, ipAddressReceived} )
+}
+
+const forbidedIpTrackRequest = ( req = request, res = response) => {
+  res.status(404).send('<h1>Resource Does Not Exist</h1>');
+}
+
+module.exports = { 
+  ipTrackRequest,
+  forbidedIpTrackRequest  
+}
--- routes/ipTrackRequest.js
+++ routes/ipTrackRequest.js
@@ -1,39 +1,10 @@
 const { Router } = require('express');
+const { ipTrackRequest, forbidedIpTrackRequest } = require('../controllers/ipTrackRequest');
 
 const router = Router();
 
-const provitionalData = {
-  ip: "8.8.8.8",
-  location: {
-      country: "US",
-      region: "California",
-      city: "Mountain View",
-      lat: 37.40599,
-      lng: -122.078514,
-      postalCode: "94043",
-      timezone: "-07:00",
-      geonameId: 5375481
-  },
-  domains: [
-      "0d2.net",
-      "003725.com",
-      "0f6.b0094c.cn",
-      "007515.com",
-      "0guhi.jocose.cn"
-  ],
-  as: {
-      asn: 15169,
-      name: "Google LLC",
-      route: "8.8.8.0/24",
-      domain: "https://about.google/intl/en/",
-      type: "Content"
-  },
-  isp: "Google LLC"
-}
-
-router.get('/', (req, res) => {
-  res.json( provitionalData );
-});
+router.get('/:ipaddress', ipTrackRequest );
+router.get('/', forbidedIpTrackRequest);
 
 
 module.exports = router;
